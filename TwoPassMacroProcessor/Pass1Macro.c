62c9329ac988bd39b88ce45f96aff065fae98f30
Alnas Kabeer <alnaskabeer@gmail.com>
2022-12-20 08:03:22 +0530

    Made Changes

--- TwoPassMacroProcessor/Pass1Macro.c
+++ TwoPassMacroProcessor/Pass1Macro.c
@@ -1,26 +1,80 @@
 #include <stdio.h>
 #include <string.h>
-void main()
-{
-    char macros[20][10], label[20], opcode[20], operand[20];
-    int i, j, n, m = 0;
-    FILE *fp1, *fp[10];
+int main() {
+    char line[256], label[20], opcode[20], operand[20];
+    char mName[20], mOper[20], arg[20];
+    FILE *finp, *mnt, *mdt, *argt, *fint;
+    finp = fopen("input.txt", "r");
+    mnt = fopen("mName.txt", "w");
+    mdt = fopen("mDef.txt", "w");
+    argt = fopen("mArg.txt", "w+");
+    fint = fopen("inter.txt", "w");
 
-    fp1 = fopen("inputm.txt", "r");
-    fscanf(fp1, "%s%s%s", label, opcode, operand);
-    while (strcmp(opcode, "END") != 0)
-    {
-        if (!strcmp(opcode, "MACRO"))
-        {
-            fp[m] = fopen(operand, "w");
-            m++;
-            fscanf(fp1, "%s%s%s", label, opcode, operand);
-            while (strcmp(opcode, "MEND") != 0)
-            {
-                fprintf(fp[m - 1], "%s\t%s\t%s\n", label, opcode, operand);
-                fscanf(fp1, "%s%s%s", label, opcode, operand);
+    int MDTC=1, MNTC=1, ARGTC=0;
+    // fgets(line, sizeof(line), finp);
+    fscanf(finp, "%s %s %s", label, opcode, operand);
+    while(strcmp(opcode,"END")){
+        if(!strcmp(opcode,"MACRO")){
+            strcpy(mName, label);
+            strcpy(mOper, operand);
+            MDTC=ftell(mdt);
+            fprintf(mnt, "%s %d\n", mName, MDTC);
+            fprintf(mdt, "%s %s ", label, opcode);
+            MNTC++;
+            strcpy(arg, "");
+            // printf("%s", arg);
+            for(int i=0, j=0; mOper[i]!='\0'; i++, j++){
+                if(mOper[i]==',' || mOper[i]=='\n'){
+                    fprintf(argt, "%d %s\n", ARGTC, arg);
+                    fprintf(mdt, "%d,", ARGTC);
+                    ARGTC++;
+                    printf("%d %s\n", ARGTC, arg);
+                    strcpy(arg, "");
+                    j=-1;
+                    continue;
+                }
+                arg[j] = mOper[i];
+                arg[j+1] = '\0';
             }
+            if(strcmp(arg, "")){
+                fprintf(argt, "%d %s\n", ARGTC, arg);
+                fprintf(mdt, "%d\n", ARGTC);
+                ARGTC++;
+                printf("%d %s\n", ARGTC, arg);
+            }
+            // MDTC++;
+            fscanf(finp, "%s %s %s", label, opcode, operand);
+            fseek(argt, 0, SEEK_SET);
+            while(strcmp(opcode,"MEND")){
+                if(operand[0]=='&'){
+                    char argnum[20],argval[20];
+                    fscanf(argt, "%s %s", argnum, argval);
+                    while(!feof(argt)){
+                        if(!strcmp(argval, operand)){
+                            strcpy(operand, argnum);
+                            break;
+                        }
+                        fscanf(argt, "%s %s", argnum, argval);
+                    }
+                }
+                fprintf(mdt, "%s %s %s\n", label, opcode, operand);
+                // MDTC++;
+                fscanf(finp, "%s %s %s", label, opcode, operand);   
+            }
+            fseek(argt, 0, SEEK_END);
+
+            fprintf(mdt, "%s %s %s\n", label, opcode, operand);
         }
-        fscanf(fp1, "%s%s%s", label, opcode, operand);
+        else {
+            fprintf(fint, "%s %s %s\n", label, opcode, operand);
+        }
+        fscanf(finp, "%s %s %s", label, opcode, operand);   
     }
+    fprintf(fint, "%s %s %s\n", label, opcode, operand);
+
+    fclose(finp);
+    fclose(mnt);
+    fclose(mdt);
+    fclose(argt);
+    fclose(fint);
 }
--- TwoPassMacroProcessor/Pass2Macro.c
+++ TwoPassMacroProcessor/Pass2Macro.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-void main()
-{
-    char macros[20][10], label[20], opcode[20], operand[20];
-    int i, j, n, m = 0;
-    FILE *fp1, *fp[10], *fp2;
-
-    fp1 = fopen("inputm.txt", "r");
-    fp2 = fopen("macro_out.txt", "w");
-    fscanf(fp1, "%s%s%s", label, opcode, operand);
-    while (strcmp(opcode, "END") != 0)
-    {
-        if (!strcmp(opcode, "CALL"))
-        {
-            fp[m] = fopen(operand, "r");
-            m++;
-            fscanf(fp[m - 1], "%s%s%s", label, opcode, operand);
-            while (!feof(fp[m - 1]))
-            {
-                fprintf(fp2, "%s\t%s\t%s\n", label, opcode, operand);
-                fscanf(fp[m - 1], "%s%s%s", label, opcode, operand);
-            }
+void main(){
+    char label[20], opcode[20], operand[20];
+    FILE *fint, *fout, *mnt, *mdt, *argt;
+    fint = fopen("inter.txt", 'r');
+    mnt = fopen("mName.txt", 'r');
+    mdt = fopen("mDef.txt", 'r');
+    argt = fopen("mArg.txt", 'r');
+    fout = fopen("output.txt", 'w');
+    fscanf(fint, "%s %s %s", label, opcode, operand);
+    char mname[20], operands[20][10];
+    int mloc;
+    while(strcmp(opcode, "END")){
+        fseek(mnt, 0, SEEK_SET);
+        fscanf(mnt, "%s %d", mname, &mloc);
+        if(!strcmp(opcode, mname)){
+            
         }
-        else
-        {
-            fprintf(fp2, "%s\t%s\t%s\n", label, opcode, operand);
-        }
-
-        fscanf(fp1, "%s%s%s", label, opcode, operand);
     }
-    fprintf(fp2, "%s\t%s\t%s\n", label, opcode, operand);
 }
