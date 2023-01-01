ae03d1165408a618515fd9f99fc25b9653b80313
Alnas Kabeer <alnaskabeer@gmail.com>
2023-01-01 21:09:10 +0530

    i'm pushinggg

---
+++ AbsoluteLoader/absolute.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<string.h>
+
+void main() {
+    FILE *finp, *fout;
+    finp = fopen("input.txt", "r");
+    fout = fopen("output.txt", "w");
+    char line[100], name[20], addr[20], len[20];
+    char lineadd[20];
+    int linecount=0;
+    int i, j;
+    fscanf(finp, "%s", line);
+    i=0; j=0;
+    int level=0;
+    while(line[i]!='\0'){
+        if(line[i]=='^'){
+            level++;
+            j=0;
+            i++;
+            continue;
+        }
+        if(level==1){
+            name[j]=line[i];
+            name[++j]='\0';
+        }
+        else if(level==2){
+            addr[j]=line[i];
+            addr[++j]='\0';
+        }
+        else if(level==3){
+            len[j]=line[i];
+            len[++j]='\0';
+        }
+        i++;
+    }
+
+    fprintf(fout, "Program Name : %s\n", name);
+    fprintf(fout, "\nStarting Address : %s\n", addr);
+    fprintf(fout, "\nLength : %s\n", len);
+
+    fscanf(finp, "%s", line);
+    while(line[0]!='E'){
+        i=0; j=0;
+        level=0;
+        while(line[i]!='\0'){
+            if(line[i]=='^'){
+                // printf("ssdf\n");
+                level++;
+                j=0;
+                i++;
+                continue;
+            }
+            if(level==1){
+                lineadd[j]=line[i++];
+                lineadd[++j]='\0';
+            }
+            else if(level==2){
+                sscanf(lineadd, "%X", &linecount);
+                i++;
+            }
+            else if(level>2){
+                fprintf(fout, "%X\t%c%c\n", linecount, line[i], line[i+1]);
+                linecount++;
+                i+=2;
+            }
+            else i++;
+        }
+        fscanf(finp, "%s", line);
+    }
+    fclose(finp);
+    fclose(fout);
+}
