8ab7111d052e5d36369bf3d122951e73d55c9533
Alnas Kabeer <alnaskabeer@gmail.com>
2023-01-01 21:26:40 +0530

    PUUSSSHH

--- RelocatingLoader/relocating.c
+++ RelocatingLoader/relocating.c
@@ -5,15 +5,16 @@ void main() {
     FILE *finp, *fout;
     finp = fopen("input.txt", "r");
     fout = fopen("output.txt", "w");
-    char line[100], name[20], addr[20], len[20];
+    char line[100], name[20], len[20];
     char lineadd[20];
-    int linecount=0;
+    int linecount=0, addr;
     int i, j;
     fscanf(finp, "%s", line);
     i=0; j=0;
     int level=0;
     while(line[i]!='\0'){
         if(line[i]=='^'){
+            printf("hee");
             level++;
             j=0;
             i++;
@@ -23,10 +24,6 @@ void main() {
             name[j]=line[i];
             name[++j]='\0';
         }
-        else if(level==2){
-            addr[j]=line[i];
-            addr[++j]='\0';
-        }
         else if(level==3){
             len[j]=line[i];
             len[++j]='\0';
@@ -34,12 +31,17 @@ void main() {
         i++;
     }
 
+    printf("Enter the starting address of the program: ");
+    scanf("%X", &addr);
+
     fprintf(fout, "Program Name : %s\n", name);
-    fprintf(fout, "\nStarting Address : %s\n", addr);
+    fprintf(fout, "\nStarting Address : %06X\n", addr);
     fprintf(fout, "\nLength : %s\n", len);
-    sscanf(addr, "%X", &linecount);
+    // sscanf(addr, "%X", &linecount);
+    linecount = addr;
     fscanf(finp, "%s", line);
     while(line[0]!='E'){
+        printf("hfd");
         i=0; j=0;
         level=0;
         while(line[i]!='\0'){
@@ -51,7 +53,7 @@ void main() {
                 continue;
             }
             else if(level>2){
-                fprintf(fout, "%X\t%c%c\n", linecount, line[i], line[i+1]);
+                fprintf(fout, "%X\t-\t%c%c\n", linecount, line[i], line[i+1]);
                 linecount++;
                 i+=2;
             }
