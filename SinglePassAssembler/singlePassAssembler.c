c77d9107e8e999c09e62f0b5fd30bba60cd44fb4
Alnas Kabeer <alnaskabeer@gmail.com>
2023-01-04 22:11:17 +0530

    update

--- SinglePassAssembler/singlePassAssembler.c
+++ SinglePassAssembler/singlePassAssembler.c
@@ -30,8 +30,15 @@ void main() {
     else {
         locctr = staddr = 0;
     }
+    fprintf(fout, "H^%s^%06X^%06X\n", name, staddr, 0);
+    fprintf(fout, "T^%06X^", locctr);
+
+    char textrec[70]="";
+    int textreclen=0;
+
     while(strcmp(opcode, "END")){
-        char curopr[20]="0000";
+        char curopc[20]="",curopr[20]="0000", curobjc[20]="";
+        int objlen;
         fseek(fsym, 0, SEEK_SET);
         fscanf(fsym, "%s %s", symlab, symadd);
         while(!feof(fsym)){
@@ -47,14 +54,15 @@ void main() {
         }
         fseek(fopc, 0, SEEK_SET);
         fscanf(fopc, "%s %s", optmne, optcod);
-        char curopc[20]="";
         while(!feof(fopc)){
             if(!strcmp(opcode, optmne)){
                 strcpy(curopc, optcod);
                 if(!strcmp(curopr, "0000")){
                     fprintf(fsnf,"%s %X\n", operand, locctr+1);
                 }
-                fprintf(fint, "%06X\t%s\t%s\t%s\t%s%s\n", locctr, label, opcode, operand, curopc, curopr);
+                strcpy(curobjc, curopc);
+                strcat(curobjc, curopr);
+                fprintf(fint, "%06X\t%s\t%s\t%s\t%s\n", locctr, label, opcode, operand, curobjc);
                 locctr+=3;
                 break;
             }
@@ -80,43 +88,32 @@ void main() {
                 }
                 curopr[byteindex] = '\0';
             }
+            strcpy(curobjc, curopr);
             fprintf(fint, "%06X\t%s\t%s\t%s\t%s\n", locctr, label, opcode, operand, curopr);
             locctr += strlen(operand)-3;
         }
         else if(!strcmp(opcode, "WORD") ){
             int wordval;
-            sscanf(operand, "%x", &wordval);
-            fprintf(fint, "%06X\t%s\t%s\t%s\t%06X\n", locctr, label, opcode, operand, wordval);
+            sscanf(operand, "%06X", &wordval);
+            sprintf(curobjc, "%06X", wordval);
+            fprintf(fint, "%06X\t%s\t%s\t%s\t%s\n", locctr, label, opcode, operand, curobjc);
             locctr += strlen(operand)-3;
+        }
+        objlen = strlen(curobjc)/2;
+        if(textreclen+objlen>30){
+            fprintf(fout, "%02X%s\n", textreclen, textrec);
+            fprintf(fout, "T^%06X^", locctr);
+            textreclen=0;
+            strcpy(textrec, "");
+        }
+        textreclen+=objlen;
+        if(strcmp(curobjc, "")){
+            sprintf(textrec,"%s^%s", textrec, curobjc);
         }
         fscanf(finp, "%s %s %s", label, opcode, operand);
     }
-    fprintf(fout, "H^%s^%06X^%06X\n", name, staddr, locctr-staddr);
-    fseek(fint, 0, SEEK_SET);
-
-    char codeAddr[20], code[20];
-    char textrec[70]="";
-    int textreclen=0;
-    fscanf(fint, "%s %s %s %s %s", codeAddr, label, opcode, operand, code);
-    fprintf(fout, "T^%s^", codeAddr);
-    while(!feof(fint)){
-        if(strcmp(opcode, "RESB") && strcmp(opcode, "RESW")){        
-            if(strlen(textrec)>70){
-                fprintf(fout, "%02X%s\n", textreclen, textrec);
-                fprintf(fout, "T^%s^", codeAddr);
-                textreclen=0;
-                strcpy(textrec, "");
-            }
-            strcat(textrec, "^");
-            strcat(textrec, code);
-            textreclen+=strlen(code)/2;
-        }
-        fscanf(fint, "%s %s %s %s %s", codeAddr, label, opcode, operand, code);
-    }
-    if(strlen(textrec)<70){
+    if(strlen(textrec)>0){
         fprintf(fout, "%02X%s\n", textreclen, textrec);
-        textreclen=0;
-        strcpy(textrec, "");
     }
     fseek(fsnf, 0, SEEK_SET);
     fscanf(fsnf, "%s %s", snflab, snfadd);
